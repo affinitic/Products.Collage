311dc71bfcd34f0cbed9eb2968e5e822626ed1b4
Malthe Borch <mborch@gmail.com>
2007-06-23 09:23:14 +0000

    Added standard and album-view for ATTopic.
    svn path=/Collage/trunk/; revision=44412

--- Products/Collage/browser/views.py
+++ Products/Collage/browser/views.py
@@ -1,10 +1,24 @@
 from Products.Five.browser import BrowserView
 
-class SimpleView(BrowserView):
+from Products.CMFCore.utils import getToolByName
+
+class BaseView(BrowserView):
     def test(self):
         return lambda a, b, c: a and b or c
 
-class RowView(SimpleView):
+    def isAnon(self):
+        return getToolByName(self.context, 'portal_membership').isAnonymousUser()
+
+    def normalizeString(self):
+        return getToolByName(self.context, 'plone_utils').normalizeString
+
+    def mtool(self):
+        return getToolByName(self.context, 'portal_membership')
+
+    def portal_url(self):
+        return getToolByName(self.context, 'portal_url')()
+
+class RowView(BaseView):
     def getColumnBatches(self, bsize=3):
         columns = self.context.getFolderContents()
         if not columns:
@@ -36,9 +50,14 @@ class RowView(SimpleView):
 class StandardRowView(RowView):
     title = u'Automatic'
 
-class StandardDocumentView(SimpleView):
+class StandardDocumentView(BaseView):
     title = u'Standard'
 
 class FeaturedDocumentView(StandardDocumentView):
     title = u'Featured'
 
+class StandardTopicView(BaseView):
+    title = u'Standard'
+
+class AlbumTopicView(BaseView):
+    title = u'Album'

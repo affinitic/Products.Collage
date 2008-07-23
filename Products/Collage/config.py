0bc04e78b6c8e2ac5d41bd146002cbb7e7fb6260
Gilles Lenfant <gilles.lenfant@gmail.com>
2008-07-23 15:57:06 +0000

    minor refactoring: we can't honour CMF 1.4 compat any more
    svn path=/Collage/trunk/; revision=68880

--- Products/Collage/config.py
+++ Products/Collage/config.py
@@ -1,10 +1,6 @@
 PROJECTNAME = "Collage"
 
-# BBB for CMF 1.4
-try:
-    from Products.CMFCore.permissions import setDefaultRoles
-except ImportError:
-    from Products.CMFCore.CMFCorePermissions import setDefaultRoles
+from Products.CMFCore.permissions import setDefaultRoles
 
 DEFAULT_ADD_CONTENT_PERMISSION = "Add Collage content"
 setDefaultRoles(DEFAULT_ADD_CONTENT_PERMISSION, ('Manager', 'Owner',))

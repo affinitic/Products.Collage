fb4933a20cd60ed056cd522f99ca243ceecc385f
Jens W. Klein <jens@bluedynamics.com>
2009-03-25 09:50:10 +0000

    in Plone 3 a contributor usally add content
    svn path=/Products.Collage/trunk/; revision=83112

--- Products/Collage/config.py
+++ Products/Collage/config.py
@@ -10,7 +10,7 @@ COLLAGE_TYPES = ('Collage', 'CollageRow', 'CollageColumn', 'CollageAlias')
 from Products.CMFCore.permissions import setDefaultRoles
 
 DEFAULT_ADD_CONTENT_PERMISSION = "Add Collage content"
-setDefaultRoles(DEFAULT_ADD_CONTENT_PERMISSION, ('Manager', 'Owner'))
+setDefaultRoles(DEFAULT_ADD_CONTENT_PERMISSION, ('Manager', 'Owner', 'Contributor'))
 
 import os
 PACKAGE_HOME = os.path.dirname(os.path.abspath(__file__))

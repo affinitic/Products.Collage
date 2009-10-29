10f6e1aa897a8dc4b4c786f9b91d6e722ff8ce31
Gilles Lenfant <gilles.lenfant@gmail.com>
2009-10-29 17:28:28 +0000

    * On alias target removal, the alias layout is reset to "standard". This gives a   chance to content authors to delete the alias, rather tha saying "Error:   Layout not found". This fixes #63
    svn path=/Products.Collage/trunk/; revision=100497

--- Products/Collage/events.py
+++ Products/Collage/events.py
@@ -4,8 +4,12 @@
 
 from zope.component import adapter
 from zope.lifecycleevent.interfaces import IObjectModifiedEvent
+from OFS.interfaces import IObjectWillBeRemovedEvent
+from zope.app.container.interfaces import IObjectRemovedEvent
+from Products.Archetypes.interfaces import IReferenceable
 from Products.CMFCore.interfaces import IContentish
 from Products.Collage.interfaces import ICollageAlias
+from Products.Collage.interfaces import IDynamicViewManager
 
 @adapter(ICollageAlias, IObjectModifiedEvent)
 def updateCollageAliasLayout(context, event):
@@ -25,4 +29,16 @@ def reindexOnModify(content, event):
     if collage:
         # Change done in a Collage subobject
         collage.reindexObject()
+    return
+
+@adapter(IContentish, IObjectWillBeRemovedEvent)
+def resetAliasLayout(content, event):
+    """We must reset the layout if the alias target is deleted"""
+    if not IReferenceable.providedBy(content):
+        return
+    aliases = content.getBRefs(relationship='Collage_aliasedItem')
+    for alias in enumerate(aliases):
+        # Reseting the layout
+        manager = IDynamicViewManager(alias)
+        manager.setLayout(u'standard')
     return

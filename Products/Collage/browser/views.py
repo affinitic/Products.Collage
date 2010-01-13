4a70da390595f40dd0f6ac955c28af269a753a78
Malthe Borch <mborch@gmail.com>
2010-01-13 11:02:01 +0000

    Added topic views for tabular and summary; a new view has also been added which inherits the view setting from the topic content object.
    svn path=/Products.Collage/trunk/; revision=108553

--- Products/Collage/browser/views.py
+++ Products/Collage/browser/views.py
@@ -2,10 +2,15 @@
 # $Id$
 
 from zope.component import getMultiAdapter
+from zope.component import getSiteManager
+from zope.component import ComponentLookupError
+from zope.interface import providedBy
+from zope.interface import Interface
 from plone.memoize.view import memoize_contextless
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
 from Products.Collage.interfaces import IDynamicViewManager, IPortletSkin
+from Products.Collage.interfaces import ICollageBrowserLayer
 
 def test(condition, value_true, value_false):
     if condition:
@@ -15,6 +20,7 @@ def test(condition, value_true, value_false):
 
 class BaseView(BrowserView):
     hide = False
+    __alias__ = None
 
     def test(self):
         return test
@@ -66,17 +72,18 @@ class BaseView(BrowserView):
 
 class ErrorViewNotFoundView(BaseView):
     title = u'View not Found'
-    hide = True    
+    hide = True
+    notfoundlayoutname = None
 
 class RowView(BaseView):
 
     def getColumnBatches(self, bsize=3):
         """Rows with more than *bsize* columns are split.
-        
+
         @param bsize: number of max. allowed columns per row. 0 for no batching.
-                
+
         @return: list of columns, each containing a list of rows.
-        """   
+        """
         columns = self.context.folderlistingFolderContents()
         if not columns:
             return []
@@ -124,6 +131,55 @@ class PortletView(BaseView):
 
 class AlbumTopicView(BaseView):
     title = u'Album'
+
+class SummaryTopicView(BaseView):
+    title = u'Summary'
+
+class TabularTopicView(BaseView):
+    title = u'Tabular'
+
+class InheritTopicView(BaseView):
+    """Inherits view from topic's display setting."""
+
+    title = u'Inherit'
+
+    mapping = {
+        'folder_listing': 'standard',
+        'folder_summary_view': 'summary',
+        'folder_tabular_view': 'tabular',
+        'atct_album_view': 'album',
+        'atct_topic_view': 'standard',
+        }
+
+    error_view_name = "error_collage-view-not-found"
+    fallback_view_name = "fallback"
+
+    def __call__(self):
+        """Determine inherited view and attempt to find suitable
+        collage view.
+
+        If a view can't be determined, render fallback view. If the
+        view does not exist, render an error message.
+        """
+
+        layout = self.context.getLayout()
+        name = self.mapping.get(layout, self.fallback_view_name)
+        spec = providedBy(self.context), ICollageBrowserLayer
+        lookup = getSiteManager().adapters.lookup
+        factory = lookup(spec, Interface, name=name)
+        if factory is None:
+            name = None
+            factory = lookup(spec, Interface, name=self.error_view_name)
+            if factory is None:
+                raise ComponentLookupError(
+                    "Layout not found: %s (and unable to render error view)." % \
+                    layout)
+        view = factory(self.context, self.request)
+        if name is None:
+            view.notfoundlayoutname = layout
+
+        view.__alias__ = self.__alias__
+        return view()
 
 class ClickableView(BaseView):
     title = u'Clickable'

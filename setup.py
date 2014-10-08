43771d6715b35f7b98e5ea6199c14ed989c5516d
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-10-08 17:52:49 +0200

    Back to development: 1.3.12

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def _textOfModuleFile(filename):
     return open(filename, 'r').read().strip()
 
 setup(name='Products.Collage',
-      version='1.3.11',
+      version='1.3.12.dev0',
       description=("A product to create page compositions in Plone."),
       long_description='\n\n'.join([
           _textOfModuleFile(name)

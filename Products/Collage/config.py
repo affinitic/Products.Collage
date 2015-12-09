2680b58bece316aaad2e8e957f8622f45d4b6a57
Jens W. Klein <jk@kleinundpartner.at>
2015-12-09 11:09:51 +0100

    allow site admin add by default

--- Products/Collage/config.py
+++ Products/Collage/config.py
@@ -12,7 +12,7 @@ COLLAGE_TYPES = ('Collage', 'CollageRow', 'CollageColumn', 'CollageAlias')
 DEFAULT_ADD_CONTENT_PERMISSION = "Add Collage content"
 setDefaultRoles(
     DEFAULT_ADD_CONTENT_PERMISSION,
-    ('Manager', 'Owner', 'Contributor')
+    ('Manager', 'Owner', 'Site Administrator', 'Contributor')
 )
 
 PACKAGE_HOME = os.path.dirname(os.path.abspath(__file__))
